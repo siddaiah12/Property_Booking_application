a231d80ec89f7e52e22049de87e726d657dcda47
siddaiah12 <peddasiddaiah12@gmail.com>
2024-09-19 19:50:05 +0530

    remove app-componrnt-html-code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'real_estate_application';
+  title = 'real_estate_property_booking_application';
 }
